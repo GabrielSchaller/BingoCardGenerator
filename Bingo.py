838bd1fe0c3bd6e9aa2d487efb3265573e1cdc72
GabrielSchaller <S4schaller@gmail.com>
2024-06-10 18:51:43 +0200

    1.2
    Added resizing functionality
    Added textwrapping functionality for longer objectives
    Handled Quitting properly instead of getting errors on cmd before closing

--- Bingo.py
+++ Bingo.py
@@ -1,13 +1,17 @@
 import json
 import random
 import tkinter as tk
+import sys
 
 #TODO: Sachen die von Personen und spielmodi abhängig sind seperat behandeln
-
+#braucht mayor rewrite ... cringe. bis dahin einfach boards neu generieren falls nötig
+#Klassen würden sich inzwischen auch lohnen
 def main():
     def generate_Bingo(window):
         for deck in cardDecks:
             cardDecks[deck] = buttonsVars[deck].get()
+        global closing
+        closing = False
         window.destroy()
 
     def toggle_background(target):
@@ -16,6 +20,14 @@ def main():
         else:
             target.configure(background="white")
 
+
+    def set_wrap(event):
+        width = event.width
+        for child in window.winfo_children():
+            child.configure(wraplength = width-20)
+
+    global closing
+    closing = True
     f = open('BingoJson.json', 'r', encoding='utf-8')
     cardJson = json.loads(f.read())
     f.close()
@@ -23,17 +35,17 @@ def main():
     for deck in cardJson:
         cardDecks.update({deck: False})
     finalDeck = []
-    window = tk.Tk()
-    window.title("Bingo Settings")
+    w1 = tk.Tk()
+    w1.title("Bingo Settings")
     buttons = {}
     buttonsVars = {}
     for deck in cardDecks:
         temp = tk.IntVar()
         buttonsVars.update({deck: temp})
-        buttons.update({deck: tk.Checkbutton(window, text=deck, variable=buttonsVars[deck], onvalue=True, offvalue=False)})
+        buttons.update({deck: tk.Checkbutton(w1, text=deck, variable=buttonsVars[deck], onvalue=True, offvalue=False)})
         buttons[deck].pack()
     buttons['general'].select()
-    tk.Label(window, text = "Dimensions: ").pack()
+    tk.Label(w1, text = "Dimensions: ").pack()
     dimensions = tk.IntVar()
     dimensions.set(5)
     dimensionsOptions = [("3", 3),
@@ -46,10 +58,12 @@ def main():
         text = "Create Bingo Board",
         width = 25,
         height = 5,
-        command = lambda: generate_Bingo(window)
+        command = lambda: generate_Bingo(w1)
     )
     button.pack()
-    window.mainloop()
+    w1.mainloop()
+    if closing == True:
+        sys.exit()
     for deck in cardDecks:
         if cardDecks[deck] == True:
             temp = cardJson[deck]
@@ -61,6 +75,7 @@ def main():
             if card == otherCard:
                 finalDeck.remove(otherCard)
     i = 0
+    global window
     window = tk.Tk()
     window.title("Bingo")
     row = 0
@@ -71,17 +86,22 @@ def main():
         finalDeck.remove(temp)
         button = tk.Button(
             text=temp,
-            width=40,
+            width=30,
             height=5,
-            bg="white"
+            bg="white",
+            wraplength = 100
         )
         button.configure(command=lambda target=button: toggle_background(target))
         bingoBoard.update({temp: button})
-        button.grid(row=row, column=column, padx=5, pady=5)
+        button.grid(row=row, column=column, padx=5, pady=5, sticky="news")
+        tk.Grid.rowconfigure(window, row, weight=1)
         row += 1
         if row == dimensions.get():
+            tk.Grid.columnconfigure(window, column, weight=1)
             row = 0
             column += 1
+        if i == 0:
+            button.bind("<Configure>", set_wrap)
         i += 1
     window.mainloop()
     main()
